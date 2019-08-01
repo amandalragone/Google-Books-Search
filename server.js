cef7208b98fb359be1d1aacd90474928fe73de52
unknown <amandalragone@gmail.com>
2019-08-01 15:30:20 -0500

    Trying to fix Mongo connection issue.

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines",
+  process.env.MONGODB_URI || "mongodb://localhost/googlebooksreact",
   {
     useCreateIndex: true,
     useNewUrlParser: true
